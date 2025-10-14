a402f8854018486d9b842838554406d3b6c89d82
Akash Kumar Singh <akkisingh952380@gmail.com>
2025-10-15 02:21:21 +0530

    fixed bugs

--- server/index.js
+++ server/index.js
@@ -1,12 +1,28 @@
 import express from "express";
 import dotenv from "dotenv";
-
-
-dotenv.config({})
+import cookieParser from "cookie-parser";
+import cors from "cors";
 import connectDB from "./database/db.js";
+import userRoute from "./routes/user.route.js";
+dotenv.config({});
+
+// call database connection here
 connectDB();
-const app=express();
-const PORT=process.env.PORT||3000;
-app.listen(PORT,()=>{
-    console.log(`server listening on port ${PORT}`);
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+// default middleware
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+}));
+
+app.use("/api/v1/user", userRoute);
+app.listen(PORT, () => {
+    console.log(`Server listen at port ${PORT}`);
 })
+ 
